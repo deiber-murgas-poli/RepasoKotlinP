16b946d39c10a5fd6deda3e69ce23155310196d9
Dmurgas <dmurgas@poligran.edu.co>
2025-09-05 20:28:24 -0500

    Repasao Kotlin

---
+++ src/Main.kt
@@ -0,0 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    //Los comentarios de una linea son con doble slash
+    /*
+    los comentarios de mas de una linea se abren con slash asterisco y se cierran con asterisco slash
+
+
+
+
+    //VARIABLES DE TIPO IMPLICITO
+    //deduce automaticamente que tipo de dato estas colocando
+
+    //VARIABLES se declaran con la palabra "var"
+    //CONSTANTES se declaran con la palabra "val"
+    //A diferencia de JAVA en kotlin no se requiere cerrar la centencia con ;
+    println("Hola Mundo")
+    var numero = 56 //variable de tipo entero
+    var decimal = 12.3 //variable de tipo decimal
+    var texto = "Repaso kotlin" //variable de tipo String
+    val pi = 3.14 //constante
+
+    println("Variable de tipo entero ${numero}")
+    println("Variable de tipo decimal ${decimal}")
+    println("Variable de tipo String ${texto}")
+    println("Constante ${pi}")
+
+*/
+
+
+    //VARIBLES DE TIPO EXPLICITO
+    //debes declarar que tipo de variable vas a manejar
+
+    var numero: Int = 56
+    var decimal: Double = 12.3
+
+    println("variable de tipo entero ${numero}")
+    println("variable de tipo decimal ${decimal}")
+
+}
